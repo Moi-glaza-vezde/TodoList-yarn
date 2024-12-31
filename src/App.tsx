4daff8d85d1418f7ef73af9564b47a1b396cad41
Moi-glaza-vezde <moi.glaza.vezde@gmail.com>
2024-12-31 08:35:00 +0500

    made a todo list component and prototyped it

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,14 @@
 import React from 'react';
 import './App.css';
+import { TodoList } from './TdoList';
 
 function App() {
    return (
       <div className="App">
-         <TodoList />
-         {/* <div>
-            <h3>What to learn</h3>
-            <div>
-               <input />
-               <button>+</button>
-            </div>
-            <ul>
-               <li>
-                  <input type="checkbox" checked={true} /> <span>HTML&CSS</span>
-               </li>
-               <li>
-                  <input type="checkbox" checked={true} /> <span>JS</span>
-               </li>
-               <li>
-                  <input type="checkbox" checked={false} /> <span>React</span>
-               </li>
-            </ul>
-            <div>
-               <button>All</button>
-               <button>Active</button>
-               <button>Completed</button>
-            </div>
-         </div> */}
+         <TodoList title={'What to learn'} />
+         <TodoList title={'Movies'} />
       </div>
    );
 }
 
-function TodoList() {
-   return <div>TodoList</div>;
-}
 export default App;
